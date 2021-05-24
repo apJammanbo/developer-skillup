d31f7efbe2ba94f67051495e864de886c669039e
apJammanbo <apJammanbo@gmail.com>
2021-05-24 15:42:06 +0900

    counter app react 날코딩

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,23 +1,61 @@
+import React, { useState } from "react";
 import "./app.css";
 
 function App() {
+  const [result, setResult] = useState([0]);
+  const [value, setValue] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const onClick = (type) => {
+    if (type === "add" || type === "sub") {
+      if (/^[-|1-9][0-9]*$/.test(value)) {
+        const number = Number(value);
+        if (number) {
+          const currentValue = result[currentIndex];
+          const newResult = result.splice(0, currentIndex + 1);
+          if (type === "add") {
+            newResult.push(currentValue + number);
+          } else {
+            newResult.push(currentValue - number);
+          }
+          setResult(newResult);
+          setCurrentIndex(currentIndex + 1);
+        }
+      }
+      setValue(0);
+    } else if (type === "undo") {
+      setCurrentIndex(currentIndex - 1);
+    } else if (type === "redo") {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value);
+  };
+
   return (
     <div className="container">
       <div id="valuebox" className="counter">
-        0
+        {result[currentIndex]}
       </div>
-      <input id="inputbox" className="input" />
+      <input id="inputbox" className="input" value={value} onChange={handleInputChange} />
       <div className="btnGroup">
-        <button id="undoButton" className="btn" disabled>
+        <button id="undoButton" className="btn" disabled={currentIndex === 0} onClick={() => onClick("undo")}>
           Undo
         </button>
-        <button id="addButton" className="btn">
+        <button id="addButton" className="btn" onClick={() => onClick("add")}>
           +
         </button>
-        <button id="subButton" className="btn">
+        <button id="subButton" className="btn" onClick={() => onClick("sub")}>
           -
         </button>
-        <button id="redoButton" className="btn" disabled>
+        <button
+          id="redoButton"
+          className="btn"
+          disabled={currentIndex >= result.length - 1}
+          onClick={() => onClick("redo")}
+        >
           Redo
         </button>
       </div>
