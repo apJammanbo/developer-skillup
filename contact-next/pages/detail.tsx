fd8d42a8e8bbdd4a2a4f93f579a4c80a6f02d231
apJammanbo <apJammanbo@gmail.com>
2021-06-07 14:25:13 +0900

    next-grid 작업중...

--- contact-next/pages/detail.tsx
+++
@@ -1,5 +0,0 @@
-const Detail = () => {
-  return <div className="container">Detail</div>;
-};
-
-export default Detail;
--- contact-next/types/index.ts
+++
@@ -1,8 +0,0 @@
-interface IContact {
-  id: number,
-  name: string,
-  age: number,
-  detail: string
-}
-
-export type {IContact}
